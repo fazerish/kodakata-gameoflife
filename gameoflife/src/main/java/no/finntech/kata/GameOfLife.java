e2c566e4afae0b48c8ef13d184f27e71e2452138
Tommy Johansen <tomjohan>
2013-03-22 13:39:19 +0100

    Feilende testInitialization()

--- gameoflife/src/main/java/no/finntech/kata/GameOfLife.java
+++ gameoflife/src/main/java/no/finntech/kata/GameOfLife.java
@@ -1,13 +1,33 @@
 package no.finntech.kata;
 
+import java.awt.*;
+
 /**
  * Hello world!
  *
  */
 public class GameOfLife
 {
+    private boolean[][] gameBoard;
+
+    public GameOfLife(int height, int width) {
+        gameBoard = new boolean[height][width];
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public int getSize() {
+        return gameBoard.length * gameBoard[0].length;
+    }
+
+    public List bangBig(java.util.List<Point> points) {
+        return null;  //To change body of created methods use File | Settings | File Templates.
+    }
+
+    public int getLiveCount() {
+        return 0;  //To change body of created methods use File | Settings | File Templates.
     }
 }
--- gameoflife/src/test/java/no/finntech/kata/GameOfLifeTest.java
+++ gameoflife/src/test/java/no/finntech/kata/GameOfLifeTest.java
@@ -1,13 +1,24 @@
 package no.finntech.kata;
 
+import java.awt.*;
+import java.util.Arrays;
+
 import org.junit.Test;
+
 import static org.fest.assertions.Assertions.assertThat;
 
 public class GameOfLifeTest {
 
     @Test
-    void testInitialization(){
+    public void testCreation() {
         GameOfLife gameOfLife = new GameOfLife(10, 20);
-        assertThat(gameOfLife).isEqualTo(new Integer(200));
+        assertThat(gameOfLife.getSize()).isEqualTo(200);
+    }
+
+    @Test
+    public void testInitialization() {
+        GameOfLife gameOfLife = new GameOfLife(10, 10);
+        gameOfLife.bangBig(Arrays.asList(new Point(2, 2), new Point(8, 8)));
+        assertThat(gameOfLife.getLiveCount()).isEqualTo(2);
     }
 }
